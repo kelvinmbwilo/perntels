53819c47c5449c0a504696d3d9c12aa95f0ec844
issyzac <issyzac.iz@gmail.com>
2014-10-02 17:50:01 +0300

    contact map

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -8,26 +8,37 @@
 
     <div class="wpb_wrapper widget-content contact-form-content col">
 
-            <div class="col-md-10">
+            <div class="col-md-12">
+                <div class="panel-body">
                 <b>Perntels Company Limited</b> was established and incorporated on 25th of September in 2006. Perntels established with main objective being to provide total engineering solution.<br>
                 Perntels Co Limited  is a fast growing company that provides Electrical works, Civil works and Telecommunications technical Services by giving innovative solutions to meet society
                 requirements with high professionalism in Tanzania, East Africa and the World at large.
                 <br>  Perntels Co Limited is fully registered with Contractors Registration Board (CRB) and TCRA as telecommunication service provider.<br><br>
-
+                </div>
             </div>
-            <div class="col-md-10">
+            <div class="col-md-12">
+                <div class="panel-body">
                 The company is owned by a team of technical personnel with main objectives of providing Power and Telecommunication Services in the industry including RF/TX and Civil works Surveys,
                 telecommunication systems design, Installation, Integration, Commissioning and optimization. On the other hand Perntels
                 aiming at providing Maintenance Services and support for electrical, civil works and telecommunication systems in the industries to meet the required customer satisfaction at reasonable prices, cost effective and professionalisms.
                 <br>
+                </div>
             </div>
-            <div class="col-md-10">
-                <h6>MISSION</h6>
+            <div class="col-md-12">
+                <div class="panel-heading">
+                    <h6>MISSION</h6>
+                </div>
+                <div class="panel-body">
                 Our mission is to provide high quality, reliable and timely services to our prospective
                 clients to meet International standards, at competitive prices.<br>
-                <h6>VISION</h6>
+                </div>
+                <div class="panel-heading">
+                    <h6>VISION</h6>
+                </div>
+                <div class="panel-body">
                 The company vision is to be a professional Civil Work, Building, Electrical and Telecommunications Engineering contractor in the market, a company able to provide such services most effectively, affordable and operating
                 at the highest professional engineering standards.
+                </div>
             </div>
         </div></section>
 
